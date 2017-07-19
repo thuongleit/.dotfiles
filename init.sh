c8a0ea990e2659802824409fa1000d1d31b304da
Thuong <thuong@HuyenNguyens-MacPro.local>
2017-07-19 20:30:48 +0300

    Update init.sh to auto config zsh

--- init.sh
+++ init.sh
@@ -1,6 +1,15 @@
+echo "---------- Cloning zprezto ----------"
+git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
+echo "---------- Cloning config -----------"
+git clone https://github.com/amilehu/my-macos-setup.git ~/.zsh
+
+echo "---------- Setting up ----------"
 ln -s ~/.zprezto/runcoms/zlogin ~/.zlogin
 ln -s ~/.zprezto/runcoms/zlogout ~/.zlogout
 ln -s ~/.zprezto/runcoms/zpreztorc ~/.zpreztorc
 ln -s ~/.zprezto/runcoms/zprofile ~/.zprofile
 ln -s ~/.zprezto/runcoms/zshenv ~/.zshenv
-ln -s ~/.zprezto/runcoms/zshrc ~/.zshrc
+ln -s ~/.zsh/.zshrc ~/.zshrc
+chsh -s $(which zsh)
+source ~/.zshrc
+echo "---------- YAY! Enjoy your ENV ----------"
