46785f921a0a6ac0e868add5e71e4959d6ee76b7
Thuong Le <thuongle.it@gmail.com>
2018-08-07 14:53:23 +0300

    Fix install.sh script of git plugin

--- plugins/git/install.sh
+++ plugins/git/install.sh
@@ -1,18 +1,15 @@
 #!/bin/sh
 
-function git_config {
-    if [ ! $(git config --global user.email) ]; then
-        echo "Setting up your git config"
-        echo " - What is your git author name?"
-        read -e git_authorname
-        echo " - What is your git author email?"
-        read -e git_authoremail
-        
-        git config --global user.name "$git_authorname"
-        git config --global user.email "$git_authoremail"
-        
-        echo "Git configged"
-    fi
-}
+if [ -z "$(git config --global user.email)" ]; then
+    echo "Setting up your git config global..."
+    echo " - What is your git author name?"
+    read -r git_author_name
+    echo " - What is your git author email?"
+    read -r git_author_email
 
-git_config
+    git config --global user.name "$git_author_name"
+    git config --global user.email "$git_author_email"
+
+    echo "Git configged."
+fi
+
