5bc241e0b4cfb14d72e077af7c60e9b528f555c1
Thuong Le <thuongle.it@gmail.com>
2018-08-07 10:51:11 +0300

    Improve init.sh script
    - Support install, update, uninstall processes with all preconfigured
    plugins (be configured via loading_plugins file) or the given plugin via
    the commands: install, update, uninstall of the script.
    
    - Add help menu

--- init.sh
+++ init.sh
@@ -1,86 +1,124 @@
 #!/bin/bash
 
+set -e
+
 # Setting up in HOME directory
-cd ~
+cd ~ || exit 1
+
+DOTFILE_GITHUB_REPO="https://github.com/thuongleit/.dotfiles.git"
 DOTFILE_ROOT="$(pwd -P)/.dotfiles"
 
-export $DOTFILE_ROOT
+info () {
+    printf '\r  [ \033[00;34m->\033[0m ] %s\n' "$1"
+}
 
-# active global functions when executing the script
-source $DOTFILE_ROOT/common.lib
+ask () {
+    printf '\r  [ \033[0;33m??\033[0m ] %s\n' "$1"
+}
+
+success () {
+    printf '\r\033[2K  [ \033[00;32mOK\033[0m ] %s\n' "$1"
+}
+
+fail () {
+    printf '\r\033[2K  [\033[0;31mFAIL\033[0m] %s\n' "$1"
+}
+
+is_admin() {
+   (id -Gn "$1" | grep -q -w admin) >/dev/null 2>&1
+}
+
+is_macos() {
+    test "$(uname -s)" = "Darwin"
+}
+
+check() {
+    command -v "$1" >/dev/null 2>&1
+}
+
+help() {
+    echo The script provides a simple stupid automated tool to auto-configure/update your Mac env from CLI.
+    echo
+    echo Commands:
+    echo
+    echo install $'\t\t' installs the given plugin \(or preconfigured plugins if no plugins provides\)
+    echo uninstall $'\t\t' uninstalls the given plugin \(or preconfigured plugins if no plugins provides\)
+    echo upgrade $'\t\t' upgrades the given plugin \(or preconfigured plugins if no plugins provides\)
+
+    echo
+    echo See also \"loading_plugins\" file to check/configure preconfigured plugins.
+    echo
+}
+
+clone_dotfiles_repo() {
+    cd ~ | exit 1
+    git clone "$DOTFILE_GITHUB_REPO" "$DOTFILE_ROOT" >/dev/null 2>&1
+    success "Cloned $DOTFILE_GITHUB_REPO into $DOTFILE_ROOT"
+}
+update_dotfiles_repo() {
+    cd "$DOTFILE_ROOT" || exit 1
+    git pull origin master >/dev/null 2>&1
+    success "dotfiles repo is up-to-date."
+}
 
 check_system() {
-    # Check Homebrew and install it if need
-    # if whoami is admin
-    if [ $(is_admin "$(whoami)") ]; then
-        if [ ! $(which brew) ]; then
-            echo "Installing Homebrew..."
+    info "Checking system..."
+    if is_admin "$(whoami)"; then
+        if ! check brew; then
+            info "Homebrew is missing. Installing..."
             /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-            brew doctor
-            echo
-        else
-            echo "Updating Homebrew..."
-            brew update; brew upgrade
-            echo
+            success "Homebrew is installed."
         fi
 
-        # Check Xcode Command Line Tools and install it if need
-        if [ ! $(xcode-select -p) ]; then
-            echo "Installing Xcode Command Line Tools..."
-            xcode-select --install
-            echo
+        if ! check "xcode-select"; then
+            info "Xcode Command Line Tools is missing. Installing..."
+            xcode-select --install >/dev/null
+            success "Xcode Command Line Tools is installed."
         fi
 
-        # Check git
-        if [ ! $(which git) ]; then
-            echo "Installing git..."
-            brew install git
-            echo
+        if ! check git; then
+            info "git is missing. Installing..."
+            brew install git >/dev/null
+            success "git is intalled." 
         fi
     fi
 
     # Check there is a cloned of dotfile repository?
-    local dotfile_github_repo="https://github.com/thuongleit/.dotfiles.git"
     if [ ! -d "$DOTFILE_ROOT" ]; then
-        echo "Clone dotfiles repositoty..."
-        git clone "$dotfile_github_repo" "$DOTFILE_ROOT"
-        cd "$DOTFILE_ROOT"
-        echo
+        clone_dotfiles_repo
     else
         # if the dotfiles folder is mine
-        if [ $(cd "$DOTFILE_ROOT"; git remote show origin | grep "thuongleit/.dotfiles.git") ]; then
-            echo "Updating your dotfiles repository..."
-            cd "$DOTFILE_ROOT"
-            git pull origin master
-            echo
+        cd "$DOTFILE_ROOT"; git remote show origin | grep "thuongleit/.dotfiles.git" >/dev/null
+        local repo_exists=$?
+        if [ $repo_exists -eq 0 ]; then
+            update_dotfiles_repo
         else
-            echo "Another dotfiles folder exists. Backing it up..."
-            mv "$DOTFILE_ROOT" "${DOTFILE_ROOT}.backup"
-            echo "Clone dotfiles repository..."
-            git clone "$dotfile_github_repo" "$DOTFILE_ROOT"
-            cd "$DOTFILE_ROOT"
-            echo
+            fail "Another dotfiles folder exists. Aborting..."
+            exit 1
         fi
     fi
+
+    success "System OK"
 }
 
 link_file () {
     local src=$1 dst=$2
 
-    local overwrite= backup= skip=
+    local overwrite=backup=skip=
     local action=
 
-    if [ -f "$dst" -o -d "$dst" -o -L "$dst" ]; then
-        if [ "$overwrite_all" == "false" -a "$backup_all" == "false" -a "$skip_all" == "false" ]; then
-            local currentSrc="$(readlink $dst)"
+    if [ -f "$dst" ] || [ -d "$dst" ] || [ -L "$dst" ]; then
+        if [ "$overwrite_all" == "false" ] && [ "$backup_all" == "false" ] && [ "$skip_all" == "false" ]; then
+            local currentSrc=
+            currentSrc="$(readlink "$dst")"
 
             if [ "$currentSrc" == "$src" ]; then
                 skip=true
             else
-                user "File already exists: $dst ($(basename "$src")), what do you want to do?\n\
+                user "File already exists: $dst ($(basename "$src")), what do you want to do?\\n\
                     [s]kip, [S]kip all, [o]verwite, '[O]verwrite all, [b]ack up, [B]ackup all?"
 
-                read -n 1 action
+                read -r -n 1 action
 
                 case "$action" in
                     o )
@@ -132,50 +170,173 @@ link_file () {
     fi
 }
 
-install() {
-    check_system
-    install_dotfiles
+make_symlinks() {
+    if [ $# -eq 0 ]; then
+        fail "make_symlinks: Missing input"
+    fi
 
-    local loading_plugins=$(< $DOTFILE_ROOT/loading_plugins)
+    for src in $(find -H "$1" -name '*.symlink' -not -path '*.git*')
+    do
+        dst="$HOME/$(basename "${src%.*}")"
+        link_file "$src" "$dst"
+    done
 }
 
-update() {
-    local loading_plugins=$(< $DOTFILE_ROOT/loading_plugins)
-
-    info "Run updating scripts from plugins..."
-    find ./plugins -name update.sh | while read updater; do sh
-}
-
-uninstall() {
-}
-
-install_dotfiles (){
-    info "Installing dotfiles..."
-
-    if [ "$(uname -s)" == "Darwin" ]; then
-        # Link symlink files
-        local overwrite_all=false backup_all=false skip_all=false
-
-        echo "Make symlink..."
-        for src in $(find -H "$DOTFILE_ROOT" -name '*.symlink' -not -path '*.git*')
-        do
-           dst="$HOME/$(basename "${src%.*}")"
-           link_file "$src" "$dst"
-        done
-
-        # Run install scripts of all plugins
-        echo "Run installation scripts from plugins..."
-        find ./plugins -name install.sh | while read installer ; do sh -c "${installer}" ; done
+change_shell() {
+    local current_shell_path=
+    current_shell_path="$(command -v "$1")"
+    if [ "$SHELL" != "$current_shell_path" ]; then
+        chsh -s "$current_shell_path"
+        success "Changed shell to $1."
     fi
 }
 
+install_plugin() {
+    if [ $# -eq 0 ]; then
+        fail "install_plugin: Missing input"
+        return 1
+    fi
 
+    local plugin_path="$DOTFILE_ROOT/plugins/$1"
 
-# Change shell to zsh if need
-if [ "$SHELL" != "$(which zsh)" ]; then
-    echo "Changing shell to zsh..."
-    chsh -s $(which zsh)
+    if [ ! -d "$plugin_path" ]; then
+        fail "Plugin $1 does not exist."
+        return 1
+    else
+        make_symlinks "$plugin_path"
+
+        local executing_script_path="${plugin_path}/install.sh"
+        if [ -f "$executing_script_path" ]; then
+            if [ ! -x "$executing_script_path" ]; then
+                chmod +x "$executing_script_path"
+            fi
+
+            sh -c "$executing_script_path"
+        fi
+
+        success "Installed plugin $1."
+    fi
+}
+
+update_plugin() {
+    if [ $# -eq 0 ]; then
+        fail "update_plugin: Missing input"
+        return 1
+    fi
+
+    local plugin_path="$DOTFILE_ROOT/plugins/$1"
+
+    if [ ! -d "$plugin_path" ]; then
+        fail "Plugin $1 does not exist."
+        return 1
+    else
+        make_symlinks "$plugin_path"
+
+        local executing_script_path="${plugin_path}/update.sh"
+        if [ -f "$executing_script_path" ]; then
+            if [ ! -x "$executing_script_path" ]; then
+                chmod +x "$executing_script_path"
+            fi
+
+            sh -c "$executing_script_path"
+        fi
+
+        success "Updated plugin $1."
+    fi
+}
+
+uninstall_plugin() {
+    if [ $# -eq 0 ]; then
+        fail "update_plugin: Missing input"
+        return 1
+    fi
+
+    local plugin_path="$DOTFILE_ROOT/plugins/$1"
+
+    if [ ! -d "$plugin_path" ]; then
+        fail "Plugin $1 does not exist."
+        return 1
+    else
+        # make_symlinks "$plugin_path"
+        local executing_script_path="${plugin_path}/uninstall.sh"
+        if [ -f "$executing_script_path" ]; then
+            if [ ! -x "$executing_script_path" ]; then
+                chmod +x "$executing_script_path"
+            fi
+
+            sh -c "$executing_script_path"
+        fi
+
+        success "Uninstalled plugin $1."
+    fi
+}
+
+if ! is_macos; then
+    fail "Sorry, we only support macOS at the moment."
+    exit 1
 fi
 
-echo "Done. Note that some of these changes require a logout/restart to take effect."
-success ---------- YAY! Enjoy your MAC ----------
+is_success_command=false
+if [ $# -eq 0 ] || [ "$1" = "help" ]; then
+    help
+else 
+    loading_plugins=$(< "$DOTFILE_ROOT"/loading_plugins)
+
+    overwrite_all=false backup_all=false skip_all=false
+    if [ "$1" = "install" ]; then
+        is_success_command=true
+        # check our system first to see whether dependencies have been installed or not
+        check_system
+
+        # install plugins
+        if [ -z "$2" ]; then
+            for plugin in ${loading_plugins[*]}
+            do
+                install_plugin "$plugin"
+            done
+        else
+            if ! install_plugin "$2"; then
+                is_success_command=false
+            fi
+        fi
+
+        # Change shell to zsh if need
+        # info "Changing default shell..."
+        # change_shell "zsh"
+        # success "Default shell changed."
+    elif [ "$1" = "update" ]; then
+        is_success_command=true
+
+        update_dotfiles_repo
+
+        if [ -z "$2" ]; then
+            for plugin in ${loading_plugins[*]}
+            do
+                update_plugin "$plugin"
+            done
+        else
+            if ! update_plugin "$2"; then
+                is_success_command=false
+            fi
+        fi
+    elif [ "$1" = "uninstall" ]; then
+        is_success_command=true
+        if [ -z "$2" ]; then
+            for plugin in ${loading_plugins[*]}
+            do
+                uninstall_plugin "$plugin"
+            done
+        else
+            if ! uninstall_plugin "$2"; then
+                is_success_command=false
+            fi
+        fi
+    else
+        help
+    fi
+fi
+
+if [ "$is_success_command" = "true" ]; then
+    echo "Done. Note that some of these changes require a logout/restart to take effect."
+    echo "---------- YAY! Enjoy your Mac ----------"
+fi
