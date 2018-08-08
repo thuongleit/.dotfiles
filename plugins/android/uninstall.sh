5b74a558eb57204098584ec770ec7271b905a3f2
Thuong Le <thuongle.it@gmail.com>
2018-08-08 14:43:43 +0300

    Update uninstall process of android plugin
    To uninstall related brews and brew casks when uninstall android plugin

--- plugins/android/uninstall.sh
+++ plugins/android/uninstall.sh
@@ -7,4 +7,9 @@ source "$android_plugin_path/common.sh"
 echo "Uninstalling Android SDK..."
 sdkmanager --uninstall --sdk_root="$ANDROID_HOME"
 
+brew uninstall gradle
+brew cask uninstall android-sdk
+brew cask uninstall android-studio
+brew cask uninstall android-studio-preview
+
 echo "Uninstalled Android SDK and Studio"
