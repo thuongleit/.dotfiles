a2df56f4575908d4b74ac2cc84734b0fa62c825c
Thuong Le <thuongle.it@gmail.com>
2018-11-19 10:54:51 +0200

    Use JAVA 8 as default to build android app

--- plugins/java/path.zsh
+++ plugins/java/path.zsh
@@ -1,3 +1,5 @@
 # JAVA_HOME PATH for java8
 JAVA8_HOME=/Library/Java/JavaVirtualMachines/"$(cd /Library/Java/JavaVirtualMachines || return; echo *jdk1.8*)"/Contents/Home
 
+export JAVA_HOME=$JAVA8_HOME
+export PATH=$JAVA_HOME:$PATH
