94e3a569c460cbd5e465c7b2df3eb8a43af946a0
Thuong Le <thuongle.it@gmail.com>
2018-11-19 10:55:14 +0200

    Remove track time in workspace

--- plugins/others/make_workspace.sh
+++ plugins/others/make_workspace.sh
@@ -8,25 +8,26 @@
 # The fourth pane run trackmac command to track computer activity running time
 
 new_workspace () { 
-	local session='WORKPLACE'
+	local session='workplace'
 	# set up tmux
 	tmux start-server
 
 	# create a new tmux session, starting vim from a saved session in the new window
 	tmux new-session -d -n $session 
 
-	# Split pane 1 horizontally by 65%
-	tmux split-window -h -p 35
+	# Split pane 1 horizontally by 85%
+	tmux split-window -h -p 15
 	# Split pane 2 vertically by 85%
 	tmux split-window -v -p 15
-	# Split pane 3 horizontally by 50%
-	tmux split-window -h -p 50
-
-	tmux select-pane -t 3
-	tmux clock-mode            # show clock
+	# Split pane 3 horizontally by 50% 
+	tmux split-window -h -p 100
 	
-	tmux select-pane -t 4
+    tmux select-pane -t 3
 	tmux send-keys "cd $dotfiles/modules/others;./trackmac.py" Enter   #track my working time
+	tmux select-pane -t 4
+    tmux clock-mode            # show clock
+	tmux select-pane -t 1
+    tmux send-keys "cd $work" Enter #go to my workplace
 
 	tmux selectp -t 1
 	tmux attach -t0
