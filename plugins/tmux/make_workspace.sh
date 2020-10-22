3cc63ee09cab2bf1907494d8d08ba228ac3bc367
Thuong Le <thuongle.it@gmail.com>
2020-10-22 03:05:01 +0300

    Update make_workspace.sh

--- plugins/tmux/make_workspace.sh
+++ plugins/tmux/make_workspace.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Setup a work space called `WORKSPACE` with 1 window
-# The window has 2 panes
+# The window has 3 panes
 # The first pane set at 65%, split horizontally, set path to $HOME
 # The second panel is split vertically 65% horizontally
 
@@ -16,18 +16,20 @@ new_workspace () {
 	# Split pane 1 horizontally by 85%
 	tmux split-window -h -p 15
 	# Split pane 2 vertically by 85%
-	# tmux split-window -v -p 15
+	tmux split-window -v -p 10
 	# # Split pane 3 horizontally by 50% 
 	# tmux split-window -h -p 100
 	
     # tmux select-pane -t 3
 	# tmux send-keys "cd $dotfiles/modules/others;./trackmac.py" Enter   #track my working time
-	# tmux select-pane -t 3
-    # tmux clock-mode            # show clock
-	tmux select-pane -t 0
+	tmux select-pane -t 3
+    tmux clock-mode            # show clock
+	tmux select-pane -t 1
     tmux send-keys "cd $work" Enter #go to my workplace
+	tmux select-pane -t 2
+    tmux send-keys "cd $HOME" Enter #go to HOME folder
 
-	tmux selectp -t 0
+	tmux selectp -t 1
 	tmux attach -t0
 }
 
