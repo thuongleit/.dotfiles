3febc937c14595c7260429c4c8b2af471c452906
Thuong Le <thuongle.it@gmail.com>
2021-01-28 16:42:22 +0200

    Fix insecure directories and files issue
    Fixed #3

--- plugins/zsh/install.sh
+++ plugins/zsh/install.sh
@@ -6,4 +6,8 @@ mkdir -p "$HOME/.zsh" >/dev/null 2>&1
 
 git clone https://github.com/sindresorhus/pure.git "$HOME/.zsh/pure" >/dev/null 2>&1
 
+# Fix insecure directories and files, run compaudit for list
+# https://github.com/zsh-users/zsh-completions/issues/433
+compaudit | xargs chmod g-w
+
 echo "zsh is configured."
