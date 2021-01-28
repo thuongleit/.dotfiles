2146bd93ca70f4ad6a483f19d4ff109559f6734d
Thuong <thuong@Huyens-MacBook-Pro.local>
2021-01-28 13:30:27 +0200

    Update install homebrew script
    Fixed #4

--- init.sh
+++ init.sh
@@ -68,7 +68,7 @@ check_system() {
     if is_admin "$(whoami)"; then
         if ! check brew; then
             info "Homebrew is missing. Installing..."
-            /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+            /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
             success "Homebrew is installed."
         fi
 
