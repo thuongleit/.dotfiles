890064c8ca0a71426232c2257b0871f95100c6ac
Thuong Le <thuongle.it@gmail.com>
2022-01-08 14:28:40 +0200

    add convert mp3 function

--- plugins/zsh/functions.zsh
+++ plugins/zsh/functions.zsh
@@ -205,6 +205,14 @@ function convert_video() {
   done
 }
 
+# convert webm to mp3 sound files
+function convert_mp3() {
+  for FILE in *.webm; do
+     echo -e "Processing video '\e[32m$FILE\e[0m'";
+     ffmpeg -i "${FILE}" -vn -ab 128k -ar 44100 -y "${FILE%.webm}.mp3";
+  done
+}
+
 function download_videos() {
   local link_file="$1"
   echo "Link file: $link_file"
